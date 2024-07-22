46467ad3776922e41f6860530a2e79b090195f87
ChinthalaShivamani <chinthala.shivamani@iitgn.ac.in>
2024-07-22 17:24:09 +0530

    Deployed

---
+++ script.js
@@ -0,0 +1,33 @@
+// var crsr=document.querySelector("#cursor")
+var crsrblur=document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    // crsr.style.left = dets.x+"px"
+    // crsr.style.top=dets.y+"px"
+    crsrblur.style.left = dets.x -200 +"px"
+    crsrblur.style.top=dets.y -200 +"px"
+})
+gsap.to("#nav",{
+    backgroundColor :"#000",
+    height:"85px",
+    delay: 0.5,
+    scrollTrigger:{
+        trigger :"#nav",
+        scroller:"body",
+        //markers:true,
+        start:"top -10%",
+        end :"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main", {
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger :"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end :"top -70%",
+        scrub:2
+
+    }
+})
